ed7ff3f1a83de180a75807e557468e274a478c1c
Ben Anderson <dataknut@gridcrawler.otago.ac.nz>
2019-08-15 11:45:21 +1200

    added tmeplate for Part B

---
+++ reports/partB_dataAnalysis/makeFile_dataAnalysisReport.R
@@ -0,0 +1,76 @@
+# Makes the data processing report
+# Saves result to /docs for github pages
+
+# Load package ----
+
+print(paste0("#-> Load GREENGridEECA package"))
+library(GREENGridEECA) # local utilities
+print(paste0("#-> Done "))
+
+GREENGridEECA::setup() # run setup to set repo level parameters
+
+# Load libraries needed across all .Rmd files ----
+localLibs <- c("rmarkdown",
+               "bookdown",
+               "data.table", # data munching
+               "GREENGridData", # so as not to re-invent the wheel
+               "here", # where are we?
+               "lubridate", # fixing dates & times
+               "utils" # for reading .gz files with data.table
+)
+GREENGridEECA::loadLibraries(localLibs)
+
+# Local functions (if any) ----
+
+getFileList <- function(dPath){
+  # should be fast
+  all.files <- list.files(path = dPath, pattern = ".csv.gz")
+  dt <- as.data.table(all.files)
+  dt[, fullPath := paste0(dPath, all.files)]
+  return(dt)
+}
+
+getData <- function(filesDT){
+  # https://stackoverflow.com/questions/21156271/fast-reading-and-combining-several-files-using-data-table-with-fread
+  # this is where we need drake
+  # and probably more memory
+  message("Loading ", nrow(filesDT), " files")
+  l <- lapply(filesDT$fullPath, fread)
+  dt <- rbindlist(l)
+  return(dt)
+  #setkey( dt , ID, date )
+}
+
+doReport <- function(){
+  rmdFile <- paste0(repoParams$repoLoc, "/reports/partB_dataAnalysis/dataAnalysisReport.Rmd")
+  rmarkdown::render(input = rmdFile,
+                    params = list(title = title,
+                                  subtitle = subtitle,
+                                  authors = authors),
+                    output_file = paste0(repoParams$repoLoc,"/docs/partB_dataAnalysisReport_v", version, ".html")
+  )
+}
+
+# Local parameters ----
+version <- "1.0"
+repoParams$repoLoc <- here::here()
+
+# data paths
+dPath <- repoParams$gridSpyHalfHour
+
+#> yaml ----
+title <- paste0("NZ GREEN Grid Household Electricity Demand Data")
+subtitle <- paste0("EECA Data Analysis (Part B) Report v", version)
+authors <- "Anderson, B., Dortans, C. and Jack, M."
+
+# --- Code ---
+
+filesDT <- getFileList(dPath)
+
+# remove rf_46 if it is in there by accident
+
+filesDT <- filesDT[!(fullPath %like% "rf_46")]
+
+allDataDT <- getData(filesDT) # breaks memory
+
+doReport()
