8a52ba5bc1db551ccfef917946460f4d1916aed8
Ben Anderson <dataknut@gridcrawler.otago.ac.nz>
2019-08-15 10:59:44 +1200

    added script to create halfHourly aggregates for all clean data for all households EXCEPT rf_46

---
+++ dataProcessing/makeHalfHourlyData.R
@@ -0,0 +1,71 @@
+# Makes the data processing report
+# Saves result to /docs for github pages
+
+# Load package ----
+
+print(paste0("#-> Load GREENGridEECA package"))
+library(GREENGridEECA) # local utilities
+print(paste0("#-> Done "))
+
+GREENGridEECA::setup() # run setup to set repo level parameters
+
+# Load libraries needed across all .Rmd files ----
+localLibs <- c("data.table", # data munching
+               "GREENGridData", # so as not to re-invent the wheel
+               "here", # where are we?
+               "lubridate", # fixing dates & times
+               "utils" # for reading .gz files with data.table
+)
+GREENGridEECA::loadLibraries(localLibs)
+
+# Local functions (if any) ----
+
+getFileList <- function(iPath){
+  # should be fast
+  all.files <- list.files(path = iPath, pattern = ".csv.gz")
+  dt <- as.data.table(all.files)
+  return(dt)
+}
+
+processData <- function(filesDT){
+  # https://stackoverflow.com/questions/21156271/fast-reading-and-combining-several-files-using-data-table-with-fread
+  # this is where we need drake
+  for(f in filesDT$all.files){
+    # for testing: f <- "rf_01_all_1min_data_withImputedTotal_circuitsToSum_v1.1.csv.gz"
+    s <- strsplit(f, "_all")
+    hh <- unlist(s)[1]
+    print(paste0("Processing: ", hh))
+    iFile <- paste0(iPath, f)
+    print(paste0("Loading ", iFile))
+    dt <- data.table::fread(iFile)
+    dt[, r_dateTime := lubridate::as_datetime(r_dateTime, tz = "Pacific/Auckland")] # this will be UTC unless you set this
+    dt[, r_dateTimeHalfHour := lubridate::floor_date(r_dateTime, "30 minutes")]
+    hhDT <- dt[, .(meanPowerW = mean(powerW),
+                   nObs = .N,
+                   sdPowerW = sd(powerW),
+                   minPowerW = min(powerW),
+                   maxPowerW = max(powerW)),
+               keyby = .(linkID, circuit, r_dateTimeHalfHour)]
+    ofile <- paste0(oPath, hh, "_allObs_halfHourly.csv")
+    print(paste0("Writing ", ofile))
+    data.table::fwrite(hhDT, file = ofile)
+    print("Done")
+  }
+}
+
+# Local parameters ----
+version <- "1.0"
+repoParams$repoLoc <- here::here()
+
+# data paths
+iPath <- paste0(repoParams$gridSpy,"1min/data/imputed/") # use data with imputed total load
+oPath <- paste0(repoParams$gridSpy,"halfHour/data/")
+
+# --- Code ---
+
+filesDT <- getFileList(iPath)
+
+# remove rf_46
+filesDT <- filesDT[!(all.files %like% "rf_46")]
+
+processData(filesDT)
